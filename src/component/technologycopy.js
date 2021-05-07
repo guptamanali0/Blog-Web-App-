0174fd83d90819114e9255cff95ca1a5d2707029
Manali Gupta <guptamanali0@gmail.com>
2021-05-07 17:39:38 +0530

    Save to Github

---
+++ src/component/technologycopy.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {NavLink,Link} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
+import Coursejson from '../course.json'
+import Logo1 from './../images/image3.jpg'
+import Technology from './technology.js';
+
+class Technologycopy extends Component{
+  state={
+    courselist:Coursejson
+  }
+
+    render(){
+      return(
+        <>
+            
+        <Technology  courselist={this.state.courselist}/>
+    
+
+        </>
+      )
+    }
+}
+export default Technologycopy;
