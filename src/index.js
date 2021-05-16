aa1376550d792441ea512437268fa51d96ead453
Manali Gupta <guptamanali0@gmail.com>
2021-05-16 10:33:48 +0530

    Save to Github

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './component/app';
 
 ReactDOM.render(
   <React.StrictMode>,
-    <App />
-    </React.StrictMode>,
+    <App /></React.StrictMode>,
+    
   document.getElementById('root')
 );
